59447377e9fbc1a6b535c3332a677f3dd71a4745
Daniela Silva <madanisi@outlook.com>
2020-09-29 06:31:09 -0500

    practica de bits

--- Practicas/test.c
+++ Practicas/test.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+typedef unsigned char uint8;
 void main()
 {
-    printf("Hello World");
-    printf("Soy Dani");
-    printf("Soy Paty");
+	uint8 u8valor= 0x0; 
+	u8valor|= ( 1 << 3 )|(1 << 4);
+	printf("La operación OR es: %d\n",u8valor);
+	u8valor&=~ ( 1 << 4);
+	printf("La operación AND es: %d\n",u8valor);
+	u8valor^= ( 1 << 3);
+	printf("La operación XOR es: %d\n",u8valor);
+ 
 }
