2e546f5ee04d4efc3cc07054e221f787043a78ac
Daniela Silva <madanisi@outlook.com>
2020-09-29 09:16:34 -0500

    Sel operacion

--- Practicas/test.c
+++ Practicas/test.c
@@ -3,24 +3,23 @@
 typedef unsigned char uint8;
 typedef unsigned char uint16;
 typedef unsigned char uint32;
+
+
 void main()
 {
-	uint8 u8valor= 0x0; 
-	uint16 u16valor= 0x0;
-	uint32 u32valor= 0x0;
-	uint8 u8bits;
-	uint8 u8ope;
+	uint32 u32NumBits=0;
+	uint32 u32ope=0;
+
 	printf("Escoga el tamaño de su variable 8, 16 o 32 bits: ");
-	scanf("%d", &u8bits);
-	printf("\nEl numero que escogio fue %d\n", u8bits);
-	if (u8bits==8|| u8bits==16|| u8bits==32)
+	scanf("%d", &u32NumBits);
+	if (u32NumBits == 8 || u32NumBits == 16 || u32NumBit == 32)
 	{
 		printf("Seleccione una operacion\n1)Operacion OR\n2)Operacion AND\n3)Operacion XOR\n");
-		scanf("%d", &u8ope);
+		scanf("%d", &u32ope);
 		if (u8ope >= 1 && u8ope <= 3)
-			printf("La operacion es acpetada\n");
+			printf("La operacion es aceptada\n");
 		else 
-			printf("Error: la opción escogida no se encuentra en las opciones"); 
+			printf("Error: la operacion escogida no se encuentra en las opciones"); 
 	}
 	else 
 		printf("La variable debe de ser de 8, 16 o 32 bits");
