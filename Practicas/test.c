a2be03232b02f6cf8ba8051547c28b8ca687f99a
Daniela Silva <madanisi@outlook.com>
2020-09-29 08:44:47 -0500

    Seleccione la operación
    El código se encuentra hasta la selección de operaciones

--- Practicas/test.c
+++ Practicas/test.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
 typedef unsigned char uint8;
+typedef unsigned char uint16;
+typedef unsigned char uint32;
 void main()
 {
 	uint8 u8valor= 0x0; 
-	u8valor|= ( 1 << 3 )|(1 << 4);
-	printf("La operación OR es: %d\n",u8valor);
-	u8valor&=~ ( 1 << 4);
-	printf("La operación AND es: %d\n",u8valor);
-	u8valor^= ( 1 << 3);
-	printf("La operación XOR es: %d\n",u8valor);
- 
+	uint16 u16valor= 0x0;
+	uint32 u32valor= 0x0;
+	uint8 u8bits;
+	uint8 u8ope;
+	printf("Escoga el tamaño de su variable 8, 16 o 32 bits: ");
+	scanf("%d", &u8bits);
+	printf("\nEl numero que escogio fue %d\n", u8bits);
+	if (u8bits==8|| u8bits==16|| u8bits==32)
+	{
+		printf("Seleccione una operacion\n1)Operacion OR\n2)Operacion AND\n3)Operacion XOR\n");
+		scanf("%d", &u8ope);
+		if (u8ope >= 1 && u8ope <= 3)
+			printf("La operacion es acpetada\n");
+		else 
+			printf("Error: la opción escogida no se encuentra en las opciones"); 
+	}
+	else 
+		printf("La variable debe de ser de 8, 16 o 32 bits");
 }
