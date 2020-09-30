d5f1caaebbda4548c684e23572e56246714f0468
Daniela Silva <madanisi@outlook.com>
2020-09-30 06:45:27 -0500

    Selección de operación
    Ya está corregido la selección

--- Practicas/test.c
+++ Practicas/test.c
@@ -8,19 +8,23 @@ typedef unsigned char uint32;
 void main()
 {
 	uint32 u32NumBits=0;
-	uint32 u32ope=0;
+	uint32 u32ope=4;
 
 	printf("Escoga el tamaño de su variable 8, 16 o 32 bits: ");
 	scanf("%d", &u32NumBits);
+	
 	if (u32NumBits == 8 || u32NumBits == 16 || u32NumBits == 32)
 	{
-		printf("Seleccione una operacion\n1)Operacion OR\n2)Operacion AND\n3)Operacion XOR\n");
-		scanf("%d", &u32ope);
-		if (u32ope >= 1 && u32ope <= 3)
-			printf("La operacion es aceptada\n");
-		else 
-			printf("Error: la operacion escogida no se encuentra en las opciones"); 
+		while (u32ope > 3)
+		{
+		    printf("Seleccione una operacion\n1)Operacion OR\n2)Operacion AND\n3)Operacion XOR\n");
+		    scanf("%d", &u32ope);
+		    if (u32ope >= 1 && u32ope <= 3)
+			    printf("La operacion es aceptada\n");
+		    else 
+			    printf("Error: la operacion escogida no se encuentra en las opciones"); 
+	    }
 	}
-	else 
-		printf("La variable debe de ser de 8, 16 o 32 bits");
+	    else 
+		    printf("La variable debe de ser de 8, 16 o 32 bits");
 }
