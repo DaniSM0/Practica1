2b90290ca819aaa3ba4ccb24d1919369ef68a4db
Daniela Silva <madanisi@outlook.com>
2020-09-30 19:13:26 -0500

    Incomplete Corrimiento de variable

--- Practicas/test.c
+++ Practicas/test.c
@@ -8,19 +8,32 @@ typedef unsigned char uint32;
 void main()
 {
 	uint32 u32NumBits=0;
-	uint32 u32ope=4;
+	uint32 u32Ope=4;
+	uint32 u32CorVar=0;
 
-	printf("Escoga el tamaño de su variable 8, 16 o 32 bits: ");
+	printf("Escoga el tamano de su variable 8, 16 o 32 bits: ");
 	scanf("%d", &u32NumBits);
 	
 	if (u32NumBits == 8 || u32NumBits == 16 || u32NumBits == 32)
 	{
-		while (u32ope > 3)
+		while (u32Ope > 3 )
 		{
-		    printf("Seleccione una operacion\n1)Operacion OR\n2)Operacion AND\n3)Operacion XOR\n");
-		    scanf("%d", &u32ope);
-		    if (u32ope >= 1 && u32ope <= 3)
-			    printf("La operacion es aceptada\n");
+		    printf("Seleccione una operacion\n");
+		    printf("1)Operacion OR\n");
+		    printf("2)Operacion AND\n");
+		    printf("3)Operacion XOR\n");
+		    scanf("%d", &u32Ope);
+		    if (u32Ope >= 1 && u32Ope <= 3)
+		    {
+			    printf("Corrimiento para la operacion: ");
+			    scanf("%d\n", &u32CorVar);
+			    if (u32CorVar > u32NumBits)
+			    	printf("Error: El numero es mayor al numero de bits");
+			    else 
+			    {
+			    	printf("Realizar la operación");
+			    }
+			}
 		    else 
 			    printf("Error: la operacion escogida no se encuentra en las opciones"); 
 	    }
