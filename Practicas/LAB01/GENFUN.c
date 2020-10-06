80f308361de048d2a4ee68474479a20002222b3e
Daniela Silva <madanisi@outlook.com>
2020-10-06 10:44:06 -0500

    Get Ocurrence

--- Practicas/LAB01/GENFUN.c
+++ Practicas/LAB01/GENFUN.c
@@ -3,6 +3,8 @@
 
 void main (void)
 {
+	uint8 u8Result=0;
+	uint8 u8Target='a';
 
 	uint8 au8ListArray[11] = "dani y paty";
 	printf("Testing vCaps_On\n");
@@ -12,6 +14,9 @@ void main (void)
 	printf("Testing vCaps_Off\n");
 	GENFUN_vCapsOff (&au8ListArray[0], 11);
 	printf("After testing: %s\n\n", au8ListArray);
+	printf("Testing GetOccurence\n");
+	u8Result = GENFUN_u8GetOccurence ( &au8ListArray[0], u8Target, 11 );
+	printf("%d", u8Result);
 
 }
 
@@ -54,5 +59,24 @@ void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList)
 
 uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList) 
 {
-	
+	uint8 u8NumTar=0;
+	while (u8SizeOfList !=0 )
+	{
+		if ( *pu8Src == u8Target )
+		{
+			u8NumTar++;
+		}
+		else
+		{
+			/*Nothing To Do*/
+		}
+		pu8Src++;
+		u8SizeOfList--;
+	}
+	return u8NumTar;
+}
+
+uint8 GENFUN_u8GetAverage (uint8 *pu8Src, uint8 u8SizeOfList)
+{
+
 }
